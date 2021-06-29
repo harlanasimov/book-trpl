4a5f99c27ac55c6aa013d178811bdc11214b84f0
Yuangui Wen <wenyuangui@yeah.net>
2021-06-29 09:41:43 +0800

    first commit

---
+++ variables/src/main.rs
@@ -0,0 +1,10 @@
+
+const MAX_POINT:u32 = 10_000;
+
+fn main() {
+    println!("The value of MAX_POINT is {}!", MAX_POINT);
+    let mut x = 5;
+    println!("The value of x is {}!", x);
+    x = 6;
+    println!("The value of x is {}!", x);
+}
